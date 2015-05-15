fdc80723ecdfe85faf43eeb1f6819f43a55eb5a4
Alex <sleepagain123@163.com>
2015-05-15 15:49:31 +0800

    commit shouting house

---
+++ Island/Assets/Scripts/CheckCation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckCation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Player") {
+            Shout.canThrow = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.tag == "Player") {
+            Shout.canThrow = false;
+        }
+    }
+}
--- Island/Assets/Scripts/Shout.cs
+++ Island/Assets/Scripts/Shout.cs
@@ -6,6 +6,10 @@ public class Shout : MonoBehaviour {
 
     public Rigidbody coconutBullet;
 
+    public static bool canThrow = false;
+
+    
+
     AudioSource soundPlayer;
 	// Use this for initialization
 	void Start () {
@@ -15,12 +19,14 @@ public class Shout : MonoBehaviour {
     
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1") && canThrow) {
             soundPlayer.Play();
             Rigidbody bullet = Instantiate(coconutBullet, transform.position, transform.rotation) as Rigidbody;
             bullet.velocity = transform.forward * 30;
-            Destroy(bullet, 1.0f);
+            //Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), bullet.GetComponent<Collider>(), true);
         }
 	}
+
+    
 
 }
